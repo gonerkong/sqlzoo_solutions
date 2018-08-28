860ceed65aec96619fd9975e77dbc2957c85a605
gonerkong <35208719+gonerkong@users.noreply.github.com>
2018-08-28 14:00:59 +0800

    Update SELECT-basics.sql

--- SELECT-basics.sql
+++ SELECT-basics.sql
@@ -42,7 +42,7 @@ The example below shows countries with an area of 250,000-300,000 sq. km.
 Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
 */
 SELECT
-	NAME,
+	name,
 	area
 FROM
 	world
