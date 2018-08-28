fd0b2367f33198508e2f4954c6829e2771c5a7fc
gonerkong <35208719+gonerkong@users.noreply.github.com>
2018-08-28 14:48:04 +0800

    Update SELECT-from-nobel.sql

--- SELECT-from-nobel.sql
+++ SELECT-from-nobel.sql
@@ -54,7 +54,7 @@ Show all details (yr, subject, winner) of the Literature prize winners for 1980 
 */
 SELECT yr, subject, winner
 FROM nobel
-WHERE (yr between '1980' and '1989') and subject = 'Literature';
+WHERE (yr BETWEEN 1980 AND 1989) AND subject = 'Literature'
 
 
 
@@ -69,7 +69,8 @@ Show all details of the presidential winners:
 	* Jimmy Carter
 	* Barack Obama
 */
-SELECT * FROM nobel
+SELECT * 
+FROM nobel
 WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter','Barack Obama')
 
 
@@ -94,7 +95,7 @@ Show the year, subject, and name of Physics winners for 1980 together with the C
 */
 SELECT yr, subject, winner
 FROM nobel
-WHERE (yr = '1980' AND subject = 'Physics') OR (yr = '1984' AND subject = 'Chemistry')
+WHERE (yr = 1980 AND subject = 'Physics') OR (yr = 1984 AND subject = 'Chemistry')
 
 
 
@@ -106,7 +107,7 @@ Show the year, subject, and name of winners for 1980 excluding Chemistry and Med
 */
 SELECT *
 FROM nobel
-WHERE yr = '1980' AND subject NOT IN ('Chemistry', 'Medicine')
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
 
 
 
@@ -119,7 +120,7 @@ together with winners of a 'Literature' prize in a later year (after 2004, inclu
 */
 SELECT *
 FROM nobel
-WHERE (subject = 'Medicine' AND yr < '1910') OR (subject = 'Literature' AND yr >= '2004')
+WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004)
 
 
 
@@ -171,5 +172,8 @@ Show the 1984 winners and subject ordered by subject and winner name; but list C
 */
 SELECT winner, subject
 FROM nobel
-WHERE yr = '1984'
+WHERE yr = 1984
 ORDER BY(subject IN ('Chemistry', 'Physics')), subject, winner
+
+
+
